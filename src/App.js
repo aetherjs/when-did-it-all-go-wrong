2d5ab9c83b86662609c787c09d2c621dc92e3810
Vlad <editedity@gmail.com>
2022-06-20 14:38:42 +0200

    Header rework, add active links

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import Clock from './components/clock/clock';
 import Header from './components/header/header';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+const headerTheme = createTheme({
+  typography: {
+    fontFamily: [
+      'Joan', 
+      'serif'
+    ].join(','),
+  },
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <ThemeProvider theme={headerTheme}>
+          <Header/>
+        </ThemeProvider>
         <div className="clock">
           <Clock/>
         </div>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,34 +1,27 @@
 import * as React from "react";
 
-// importing material UI components
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Header() {
     return (
         <AppBar 
             position="sticky"
-            color="grey"
-        >
-            <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h5"
-                    component="div" sx={{ flexGrow: 1 }}>
+            color="grey">
+            <Toolbar sx={{ justifyContent: "space-between" }}>
+                <Typography variant="h4"
+                    component="div" sx={{ justifyContent: "center"}} >
                     When Did It All Go Wrong?
                 </Typography>
-                <Button color="inherit">Donate</Button>
+                <Button 
+                    variant="outlined" 
+                    color="inherit"
+                    href="https://github.com/dkuznetsov/help-ukraine"
+                    float="right">
+                    How To Help
+                </Button>
             </Toolbar>
         </AppBar>
     );
